1d3325e4544bb43209d23d931d8ea1f4ef7f7c11
yugojyoti <yugojyotimohanta@gmail.com>
2023-09-09 07:49:22 +0530

    bug fix const base utl

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL =
+  process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
 
 export const USERS_URL = "/api/users";
 
